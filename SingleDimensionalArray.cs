57c366dafcf1955613c37a0328f6642dd2180b77
Jayesh267 <jayeshmhatre267@gmail.com>
2022-11-15 17:15:52 -0500

    Update and rename Arrays.cs to SingleDimensionalArray.cs

---
+++ SingleDimensionalArray.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Udemy
+{
+    public class SingleDimensionalArray
+    {
+        public int[] arr = null;
+
+        public SingleDimensionalArray (int sizeOfArray)
+        {
+            arr = new int[sizeOfArray];
+            for (int i=0; i< arr.Length; i++)
+            {
+                arr[i] = Int32.MinValue;
+            }
+        }
+
+        public void insert (int location, int valueToBeAdded)
+        {
+            try
+            {
+                if (arr[location] == Int32.MinValue)
+                {
+                    arr[location] = valueToBeAdded;
+                    Console.WriteLine("Item successfully added");
+                }
+                else
+                {
+                    Console.WriteLine("This cell is already occupied");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Index out of range");
+            }
+        }
+
+        //Array traversal
+        public void traverseArray()
+        {
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Array no longer exists!!");
+            }
+        }
+    }
+}
